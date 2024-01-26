ae400ad211f72fcc0b1f2eec5187beb4b61bca7e
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-01-26 19:36:55 +0530

    Completed the user registration

--- src/main/java/org/example/controller/LoginFormController.java
+++ src/main/java/org/example/controller/LoginFormController.java
@@ -6,20 +6,72 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.example.model.UserModel;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class LoginFormController {
+
+    @FXML
+    private AnchorPane rootNode;
+
     @FXML
     private JFXTextArea txtName;
+
+    @FXML
+    private JFXTextArea txtPassword;
+
+    private final String adminUserName = "sa";
+    private final String adminPassword = "s1022";
+
+    private String enteredUserName;
+    private String enteredPassword;
 
     public void initialize(){
     }
 
     @FXML
     void btnLoginOnAction(ActionEvent event) throws IOException {
-        if (!txtName.getText().isEmpty() && txtName.getText().matches("[A-Za-z0-9]+")) {
+        enteredUserName = txtName.getText();
+        enteredPassword = txtPassword.getText();
+
+        if (!enteredUserName.isEmpty() && enteredUserName.matches("[A-Za-z0-9]+")) {
+            try {
+                boolean useIsExist = UserModel.isExistUser(enteredUserName, enteredPassword);
+                if (useIsExist) {
+                    navigateToClientWindow();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Invalid credentials").show();
+                }
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+        } else if (enteredUserName.isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please enter your name").show();
+        }
+    }
+
+    @FXML
+    void hyperRegisterOnAction(ActionEvent event) throws IOException {
+        if (adminUserName.equals(txtName.getText()) && adminPassword.equals(txtPassword.getText())) {
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/register_form.fxml"));
+            Scene scene = new Scene(anchorPane);
+            Stage stage = (Stage) rootNode.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("User Registration");
+            stage.centerOnScreen();
+            stage.show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Invalid credentials for registration").show();
+        }
+    }
+
+    private void navigateToClientWindow() throws IOException {
+
+        if (!enteredUserName.isEmpty() && enteredUserName.matches("[A-Za-z0-9]+" )) {
             Stage primaryStage = new Stage();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/client_form.fxml"));
 
